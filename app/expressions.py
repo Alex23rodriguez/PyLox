73c5309882190aceadd5fde1ce1b3e5d93feae2a
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2024-08-08 17:14:49 +0200

    added visitor pattern to expr classes

--- app/expressions.py
+++ app/expressions.py
@@ -4,8 +4,28 @@ from dataclasses import dataclass
 from app.scanner import Token
 
 
+class Visitor(ABC):
+    @abstractmethod
+    def visitBinaryExpr(self, expr):
+        pass
+
+    @abstractmethod
+    def visitGroupingExpr(self, expr):
+        pass
+
+    @abstractmethod
+    def visitLiteralExpr(self, expr):
+        pass
+
+    @abstractmethod
+    def visitUnaryExpr(self, expr):
+        pass
+
+
 class Expr(ABC):
-    pass
+    @abstractmethod
+    def accept(self, visitor: Visitor):
+        pass
 
 
 @dataclass
@@ -14,18 +34,30 @@ class Binary(Expr):
     operator: Token
     right: Expr
 
+    def accept(self, visitor):
+        visitor.visitBinaryExpr(self)
+
 
 @dataclass
 class Grouping(Expr):
     expression: Expr
+
+    def accept(self, visitor):
+        visitor.visitGroupingExpr(self)
 
 
 @dataclass
 class Literal(Expr):
     value: float | str | bool | None
 
+    def accept(self, visitor):
+        visitor.visitLiteralExpr(self)
+
 
 @dataclass
 class Unary(Expr):
     operator: Token
     right: Expr
+
+    def accept(self, visitor):
+        visitor.visitUnaryExpr(self)
